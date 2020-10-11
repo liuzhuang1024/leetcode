0cba5c3ef7ebfbfeb5ae65fd0648b1b65b8feae4
LiuZhuang <1028741371@qq.com>
2020-10-11 17:05:35 +0800

    1011

---
+++ 01背包问题.py
@@ -0,0 +1,18 @@
+def solve():
+    dp = [[0 for i in range(W+1)] for i in range(N+1)]
+    for i in range(1, N+1):
+        for j in range(1, W+1):
+            if j-wt[i-1]<0:
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-wt[i-1]]+val[i-1])
+    return dp[N][W]
+
+
+if __name__ == "__main__":
+    N = 3
+    W = 4
+    wt = [2, 1, 3]
+    val = [4, 2, 3]
+    res = solve()
+    print(res)
---
+++ 分割等和子集.py
@@ -0,0 +1,8 @@
+def solve(arr):
+    return
+
+
+if __name__ == "__main__":
+    arr = [1, 5, 11, 5]
+    res = solve(arr)
+    print(res)
